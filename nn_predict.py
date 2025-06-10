88958c95ecf2cddf7a41fb626818a92760d4c4d3
jason6109 <j30892176@gmail.com>
2025-06-10 14:29:33 +0800

    第二次提交

--- nn_predict.py
+++ nn_predict.py
@@ -1,58 +1,10 @@
 import numpy as np
-import json
 
-# === Activation functions ===
 def relu(x):
-    # TODO: Implement the Rectified Linear Unit
-<<<<<<< HEAD
     return np.maximum(0, x)
 
 def softmax(x):
-    # TODO: Implement the SoftMax function
-    shifted_x = x - np.max(x, axis=-1, keepdims=True)
-    exp_x = np.exp(shifted_x)
+    x = x - np.max(x, axis=-1, keepdims=True)
+    exp_x = np.exp(x)
     return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
-=======
-    return x
 
-def softmax(x):
-    # TODO: Implement the SoftMax function
-    return x
->>>>>>> e9c2753da669c7aa542f5dc97bce3c5b53d5c3b1
-
-# === Flatten ===
-def flatten(x):
-    return x.reshape(x.shape[0], -1)
-
-# === Dense layer ===
-def dense(x, W, b):
-    return x @ W + b
-
-# Infer TensorFlow h5 model using numpy
-# Support only Dense, Flatten, relu, softmax now
-def nn_forward_h5(model_arch, weights, data):
-    x = data
-    for layer in model_arch:
-        lname = layer['name']
-        ltype = layer['type']
-        cfg = layer['config']
-        wnames = layer['weights']
-
-        if ltype == "Flatten":
-            x = flatten(x)
-        elif ltype == "Dense":
-            W = weights[wnames[0]]
-            b = weights[wnames[1]]
-            x = dense(x, W, b)
-            if cfg.get("activation") == "relu":
-                x = relu(x)
-            elif cfg.get("activation") == "softmax":
-                x = softmax(x)
-
-    return x
-
-
-# You are free to replace nn_forward_h5() with your own implementation 
-def nn_inference(model_arch, weights, data):
-    return nn_forward_h5(model_arch, weights, data)
-    
